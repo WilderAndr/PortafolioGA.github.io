7bda89084160ea8dc6f2067ab17d7f98c6a871b4
Wilder-Arredondo <48569015@continental.edu.pe>
2025-04-26 19:45:22 -0500

    Update orm.js

--- _assets/js/orm.js
+++ _assets/js/orm.js
@@ -1,1 +1,31 @@
+class JSONORM {
+  constructor(dataSource) {
+    this.dataSource = dataSource;
+    this.data = null;
+  }
 
+  async load() {
+    const response = await fetch(this.dataSource);
+    this.data = await response.json();
+    return this.data;
+  }
+
+  findAll() {
+    return this.data ? this.data.projects : [];
+  }
+
+  findById(id) {
+    return this.findAll().find(project => project.id === id);
+  }
+
+  findByTag(tag) {
+    return this.findAll().filter(project => 
+      project.tags.includes(tag)
+    );
+  }
+}
+
+// Uso:
+// const orm = new JSONORM('/portfolio-jekyll-tdd/_data/projects.json');
+// await orm.load();
+// const projects = orm.findAll();
