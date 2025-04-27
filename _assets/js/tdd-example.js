9be010a377318227affbf39e1e49beedc02627dd
Wilder-Arredondo <48569015@continental.edu.pe>
2025-04-26 19:45:41 -0500

    Update tdd-example.js

--- _assets/js/tdd-example.js
+++ _assets/js/tdd-example.js
@@ -1,1 +1,18 @@
+class Calculator {
+  add(a, b) {
+    return a + b;
+  }
 
+  subtract(a, b) {
+    return a - b;
+  }
+
+  multiply(a, b) {
+    return a * b;
+  }
+
+  divide(a, b) {
+    if (b === 0) throw new Error("División por cero");
+    return a / b;
+  }
+}
